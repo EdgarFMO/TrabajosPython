f404cddccc8ff6529c2121e792e0f065f642e3f2
Edgar Moreno <>
2021-04-22 20:00:46 +0000

    Reto 4 SM

---
+++ py2_retos1/reto4.py
@@ -0,0 +1,9 @@
+x = input('Escriba un numero aleatorio : ')
+y = input('Escriba otro numero aleatorio : ')
+
+if x > y :
+  print(x,'es mayor que',y)
+elif x < y :
+  print( y,'es mayor que',x)
+elif x==y :
+  print(x,'y ',y,'son iguales')  
