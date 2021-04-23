b249b3dd0e12a25349efccc4197e25f8bf65f51b
Edgar Moreno <>
2021-04-23 00:19:55 +0000

    Reto 14 SM

---
+++ py2_retos1/reto14.py
@@ -0,0 +1,6 @@
+import random
+def cua(aber):
+  cuadra = aber ** 2
+  print('El numero aleatorio escogido para calcular el cuadrado fué : ',aber)
+  print('El cuadrado del numero ',aber,'es',cuadra )
+cua(random.randint(-10,10))
