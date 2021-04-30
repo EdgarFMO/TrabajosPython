15ad2149c1143f4e932ec0d6feecdd12e17de7a3
Edgar Moreno <>
2021-04-30 16:26:47 +0000

    Nuevo folder

---
+++ py4_compresion/lista.py
@@ -0,0 +1,15 @@
+def sumatoria_2(A,B):
+
+  n=len(A)//2
+  acumulador=0
+  for i in range(n):
+    d = (A[i+1])**2
+    e = d+B[n+1]
+    acumulador = acumulador + e
+  print(acumulador)
+  return(acumulador) 
+A = [4,6,8]
+B = [2,2,2]  
+
+sumatoria_2(A,B)
+   
---
+++ py4_compresion/sumatoria.py
@@ -0,0 +1,9 @@
+A = [2,3,4]
+B = [3,6,5]
+C = [2,7,6]
+
+n = len(A)
+
+x = sum(((A[i]*B[i])+ C[i])for i in range(n))+n**2
+print(x)
+   
