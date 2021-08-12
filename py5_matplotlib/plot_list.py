ac6806fe47d732cc999e9ef542d729ce8440ba81
Edgar Moreno <81333119+EdgarFMO@users.noreply.github.com>
2021-08-12 04:05:27 +0000

    continúan las correcciones

--- py5_matplotlib/plot_list.py
+++ py5_matplotlib/plot_list.py
@@ -1,14 +1,14 @@
 ndelista = int(input('Escriba el numero para la lista :  '))
 num = ndelista + 10;
 print('El número escogido es: ',num)
-
+dos = 2;
+uno = 1; 
 while num > 1 :
 
- num = num % 2;
+ #num = num % 2;
 
- if num1 == 0 :
-  num1 = num1 / 2;
-
- elif num1 == 1 :
-  num1 = ((num1*3) + 1);
-print(num1)
+ if (num % dos) == 0 :
+  num = num / 2;
+ elif (num % uno) == 1 :
+  num = ((num*3) + 1);
+ print(num)
