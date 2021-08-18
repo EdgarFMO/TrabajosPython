e4e9a01bd9cff7df4b072646af6bdf4dccf20a8d
Edgar Moreno <81333119+EdgarFMO@users.noreply.github.com>
2021-08-18 01:16:10 +0000

    reto13 sin mirar

---
+++ py6_retos2/reto13.py
@@ -0,0 +1,7 @@
+x = int(input('Escriba el primer numero al que desea hallarle la media: '))
+z = int(input('Escriba el segundo numero al que desea hallarle la media: '))
+y = x+z
+resu = y/2;
+print('La media de',x,'y',y,'es igual a: ',resu) 
+
+  
