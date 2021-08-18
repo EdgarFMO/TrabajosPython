feb9e01814a00085dc7372194d80a7db655e8ced
Edgar Moreno <81333119+EdgarFMO@users.noreply.github.com>
2021-08-18 02:02:10 +0000

    float en reto13 y reto09 sin mirar

---
+++ py6_retos2/reto09.py
@@ -0,0 +1,6 @@
+i = 0
+x = 1
+while i <= 63 :
+  x = x*2
+  i+=1
+print('El número de granos de trigo que tendríamos que utilizar es: ',x)
--- py6_retos2/reto13.py
+++ py6_retos2/reto13.py
@@ -1,5 +1,5 @@
-x = int(input('Escriba el primer numero al que desea hallarle la media: '))
-z = int(input('Escriba el segundo numero al que desea hallarle la media: '))
+x = float(input('Escriba el primer numero al que desea hallarle la media: '))
+z = float(input('Escriba el segundo numero al que desea hallarle la media: '))
 y = (x+z) / 2
 print('La media de',x,'y',z,'es igual a: ',y) 
 
