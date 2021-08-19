9bbd58e670271e2cb69dbf9600c94a712e9f692c
Edgar Moreno <81333119+EdgarFMO@users.noreply.github.com>
2021-08-19 04:40:18 +0000

    cambio de progrmas entre el reto09 y el reto10, reto09 mirando, reto 10 sin mirar

--- py6_retos2/reto09.py
+++ py6_retos2/reto09.py
@@ -1,6 +1,8 @@
-i = 0
-x = 1
-while i <= 63 :
-  x = x*2
-  i+=1
-print('El número de granos de trigo que tendríamos que utilizar es: ',x)
+x = int(input('Escriba el número del valor inicial: '))
+y = int(input('Escriba el número del valor final: '))
+z = int(input('Escriba el intervalo: '))
+total=0 #inicializamos la suma a cero
+for i in range(x,y,z):
+  total+=i #la variable total actúa de acumulador
+  print(i)
+print('La suma es:',total)
